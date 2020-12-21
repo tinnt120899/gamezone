a30380966526b5f91498ede1a6ab023a6f3388f5
tinnt120899 <52810401+tinnt120899@users.noreply.github.com>
2020-12-21 14:29:43 +0700

    add animation

--- index.js
+++ index.js
@@ -1,132 +1,285 @@
-//autor/creator: https://vincentgarreau.com/particles.js/#nasa
-
-particlesJS("particles-js", {
-        "particles": {
-            "number": {
-                "value": 400,
-                "density": {
-                    "enable": true,
-                    "value_area": 3000
-                }
-            },
-            "color": {
-                "value": "#fc0000"
-            },
-            "shape": {
-                "type": "circle",
-                "stroke": {
-                    "width": 0,
-                    "color": "#000000"
-                },
-                "polygon": {
-                    "nb_sides": 3
-                },
-                "image": {
-                    "src": "img/github.svg",
-                    "width": 100,
-                    "height": 100
-                }
-            },
-            "opacity": {
-                "value": 0.5,
-                "random": true,
-                "anim": {
-                    "enable": false,
-                    "speed": 1,
-                    "opacity_min": 0.1,
-                    "sync": false
-                }
-            },
-            "size": {
-                "value": 2,
-                "random": true,
-                "anim": {
-                    "enable": true,
-                    "speed": 5,
-                    "size_min": 0,
-                    "sync": false
-                }
-            },
-            "line_linked": {
-                "enable": false,
-                "distance": 500,
-                "color": "#ffffff",
-                "opacity": 0.4,
-                "width": 2
-            },
-            "move": {
-                "enable": true,
-                "speed": 7.81,
-                "direction": "top",
-                "random": true,
-                "straight": false,
-                "out_mode": "out",
-                "bounce": false,
-                "attract": {
-                    "enable": false,
-                    "rotateX": 600,
-                    "rotateY": 1200
-                }
+(function e(t, n, r) {
+    function s(o, u) {
+        if (!n[o]) {
+            if (!t[o]) {
+                var a = typeof require == "function" && require;
+                if (!u && a) return a(o, !0);
+                if (i) return i(o, !0);
+                var f = new Error("Cannot find module '" + o + "'");
+                throw f.code = "MODULE_NOT_FOUND", f
             }
-        },
-        "interactivity": {
-            "detect_on": "canvas",
-            "events": {
-                "onhover": {
-                    "enable": false,
-                    "mode": "bubble"
-                },
-                "onclick": {
-                    "enable": false,
-                    "mode": "repulse"
-                },
-                "resize": true
+            var l = n[o] = {
+                exports: {}
+            };
+            t[o][0].call(l.exports, function(e) {
+                var n = t[o][1][e];
+                return s(n ? n : e)
+            }, l, l.exports, e, t, n, r)
+        }
+        return n[o].exports
+    }
+    var i = typeof require == "function" && require;
+    for (var o = 0; o < r.length; o++) s(r[o]);
+    return s
+})({
+    "./src/js/index.js": [function(require, module, exports) {
+        var Particle = require('./particles'),
+            canvas = document.querySelector('#particlesField'),
+            ctx = canvas.getContext('2d'),
+            width = window.innerWidth,
+            height = window.innerHeight,
+
+            presetDefault = {
+                count: 1000,
+                size: 1,
+                minSpeed: 10,
+                maxSpeed: 50,
+                startOrigin: {
+                    x: undefined,
+                    y: undefined
+                }
             },
-            "modes": {
-                "grab": {
-                    "distance": 400,
-                    "line_linked": {
-                        "opacity": 0.5
+
+            presetFast = {
+                count: 1000,
+                size: 1,
+                minSpeed: 20,
+                maxSpeed: 100,
+                startOrigin: {
+                    x: undefined,
+                    y: undefined
+                }
+            },
+
+            presetCentralExplode = {
+                count: 1000,
+                size: 1,
+                minSpeed: 1,
+                maxSpeed: 100,
+                startOrigin: {
+                    x: width / 2,
+                    y: height / 2
+                }
+            },
+
+            presetInsaneRandomSizeFromLeftTop = {
+                count: 2000,
+                size: function() {
+                    return Math.random() * 10 + 1;
+                },
+                minSpeed: 20,
+                maxSpeed: 100,
+                startOrigin: {
+                    x: 1,
+                    y: 1
+                }
+            },
+
+            presetInsaneRandomSizeFromCenter = {
+                count: 2000,
+                size: function() {
+                    return Math.random() * 2 + 0.2;
+                },
+                minSpeed: 20,
+                maxSpeed: 100,
+                startOrigin: {
+                    x: width / 2,
+                    y: height / 2
+                }
+            },
+
+            settings = presetDefault;
+
+        window.generateParticles = function(count, size, originX, originY) {
+
+            window.particles = window.particles || [];
+
+            for (var i = 0; i <= count; i++) {
+                var x = originX || Math.random() * window.innerWidth,
+                    y = originY || Math.random() * window.innerHeight;
+                (function(particle) {
+                    window.particles.push(particle);
+                })(new Particle(x, y, size));
+            }
+        };
+
+        /* ======================= */
+
+        resize();
+
+        window.addEventListener('resize', resize, false);
+
+        generateParticles(settings.count, settings.size, settings.startOrigin.x, settings.startOrigin.y);
+
+        animate();
+
+        /* ======================= */
+
+        function resize() {
+            width = window.innerWidth;
+            height = window.innerHeight;
+
+            if (window.particles) {
+
+                for (var i = 0; i < window.particles.length; i++) {
+                    if (window.particles[i].position.x > width) {
+                        window.particles[i].stop();
+                        window.particles[i].position.x = width;
                     }
-                },
-                "bubble": {
-                    "distance": 400,
-                    "size": 4,
-                    "duration": 0.3,
-                    "opacity": 1,
-                    "speed": 3
-                },
-                "repulse": {
-                    "distance": 200,
-                    "duration": 0.4
-                },
-                "push": {
-                    "particles_nb": 4
-                },
-                "remove": {
-                    "particles_nb": 2
+
+                    if (window.particles[i].position.y > height) {
+                        window.particles[i].stop();
+                        window.particles[i].position.y = height;
+                    }
+
                 }
             }
-        },
-        "retina_detect": true
-    }
-);
-var count_particles, stats, update;
-stats = new Stats;
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-update = function () {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update)
-    ;
-};
-requestAnimationFrame(update);
 
+        }
+
+        function renderCanvas() {
+
+            ctx.globalCompositeOperation = 'destination-out';
+            ctx.fillStyle = 'rgba(0,0,0)';
+
+            ctx.fillRect(0, 0, width, height);
+
+            ctx.globalCompositeOperation = 'source-over';
+            ctx.fillStyle = "#cc3b29";
+
+            if (window.particles) {
+                for (var i = 0; i < window.particles.length; i++) {
+                    var ball = window.particles[i];
+                    ctx.beginPath();
+                    ctx.arc(ball.position.x, ball.position.y, ball.radius, 0, Math.PI * 2, false);
+                    ctx.closePath();
+                    ctx.fill();
+                }
+            }
+
+        }
+
+        function animate(time) {
+            requestAnimationFrame(animate);
+
+            if (width !== canvas.width) {
+                canvas.width = width;
+            }
+
+            if (height !== canvas.height) {
+                canvas.height = height;
+            }
+
+            if (window.particles) {
+                for (var i = 0; i < window.particles.length; i++) {
+                    var ball = window.particles[i];
+                    if (!ball.getPosition(time)) {
+                        var x = Math.random() * width,
+                            y = Math.random() * height,
+                            speed = Math.random() * (settings.maxSpeed / 2) + settings.minSpeed;
+                        ball.move(x, y, speed);
+                    }
+                }
+            }
+
+            renderCanvas();
+        }
+
+    }, {
+        "./particles": "c:\\work\\particles-animation\\src\\js\\particles\\index.js"
+    }],
+    "c:\\work\\particles-animation\\src\\js\\particles\\index.js": [function(require, module, exports) {
+        var Particle;
+
+        Particle = function(posx, posy, radius) {
+
+            if (radius < 0) {
+                throw "Ошибка! Дан радиус частицы " + radius + " пикселей, но радиус не может быть отрицательным!";
+            }
+
+            this.position = {
+                x: posx || 0,
+                y: posy || 0
+            };
+
+            if (typeof radius == 'function') {
+                this.radius = radius();
+            } else {
+                this.radius = radius || 0;
+            }
+
+            this.status = 'standing'; // Статусы: standing || moving
+
+            this.direction = this.position;
+
+            this.speed = 1; // 1 пиксель в секунду
+
+            this.spotlightTimeStamp = undefined;
+
+        };
+
+        Particle.prototype.stop = function() {
+            this.status = 'standing';
+            this.spotlightTimeStamp = undefined;
+            this.direction = this.position;
+        };
+
+        Particle.prototype.move = function(posx, posy, speed) {
+
+            this.status = 'moving';
+
+            this.spotlightTimeStamp = undefined;
+
+            var deltaX = posx - this.position.x,
+                deltaY = posy - this.position.y,
+                distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+
+            this.direction = {
+                x: posx,
+                y: posy,
+                distance: distance,
+                sin: deltaY / distance,
+                cos: deltaX / distance
+            };
+
+            this.startPoint = this.position;
+
+            this.speed = speed || 1;
+
+        };
+
+        Particle.prototype.getPosition = function getPosition(movetime) {
+
+            var time = movetime / 1000;
+
+            if (this.status == 'moving') {
+                if (this.spotlightTimeStamp) {
+                    var deltaTime = time - this.spotlightTimeStamp,
+                        distance = (deltaTime * this.speed);
+
+                    var posy = this.direction.sin * distance,
+                        posx = this.direction.cos * distance;
+
+                    this.position = {
+                        x: posx + this.startPoint.x,
+                        y: posy + this.startPoint.y
+                    };
+
+                    if (distance > this.direction.distance) {
+                        this.status = 'standing';
+                        this.spotlightTimeStamp = undefined;
+                        this.position = this.direction;
+                    }
+
+                } else {
+                    this.spotlightTimeStamp = time;
+                }
+                return this.position;
+            } else {
+                return false;
+            }
+        };
+
+        module.exports = Particle;
+    }, {}]
+}, {}, ["./src/js/index.js"])
